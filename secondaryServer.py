7bdfb2d9044c6a86477373af014e0df1643cf5c5
William Denorme <80251305+Wonkruu@users.noreply.github.com>
2022-10-25 16:06:18 +0200

    Feat/secondary server watchdog link
    * wip watchdog connection secondary server
    
    * resolve .gitignore file problems
    
    * watchdog secondary server ok
    
    Co-authored-by: Aves <carlier.amandine98@gmail.com>

--- secondaryServer.py
+++ secondaryServer.py
@@ -3,11 +3,18 @@
 #
 # Version 30/09/2022
 #
-import os, sys
+import os, sys, socket, time
 from multiprocessing import shared_memory
 
 
-def secondaryServerBehavior(sharedMemory, pathTube1, pathTube2):
+def secondaryServerBehavior(sharedMemory, pathTube1, pathTube2, host, secondaryPort):
+    sharedMemorySecondaryServer(sharedMemory, pathTube1, pathTube2)
+    communicationWatchDog(host, secondaryPort)
+    sys.exit(0)
+    
+
+
+def sharedMemorySecondaryServer(sharedMemory, pathTube1, pathTube2):
     try:
         sharedMemoryC = shared_memory.SharedMemory(sharedMemory.name)
         print('Taille du segment mémoire partagée en octets via second accès :', len(sharedMemoryC.buf[:len(sharedMemory.buf)]))
@@ -33,6 +40,40 @@ def secondaryServerBehavior(sharedMemory, pathTube1, pathTube2):
         os.wait()
         sharedMemoryC.close()
         sharedMemoryC.unlink()
-        sys.exit(0)
     except OSError as error:
         print("Error:", error)
+
+
+def communicationWatchDog(hostWatchDog, portWatchDog):
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        mySocket.bind((hostWatchDog, portWatchDog))
+    except socket.error:
+        print('\nImpossible d\'établir la liaison du socket à l\'adresse choisie ({}:{}) -- Parent !\n'.format(hostWatchDog, portWatchDog))
+        sys.exit()
+    while True:
+        print('Serveur prêt, en attente de requêtes sur {}:{}.. -- Parent.'.format(hostWatchDog, portWatchDog))
+        mySocket.listen(5)
+
+        connexion, address = mySocket.accept()
+        print('Client connecté, adresse IP %s, port %s' % (address[0], address[1]))
+
+        connexion.send(bytes('Connected to server','UTF-8'))
+        while True:
+            msgClientraw = connexion.recv(1024)
+            msgClient = msgClientraw.decode('UTF-8')
+            print('watch dog>', msgClient)
+            if msgClient.upper() == "FIN":
+                break
+            msgServeur = bytes('Server> Connexion ok', 'UTF-8')
+            connexion.send(msgServeur)
+            time.sleep(2)
+
+        connexion.send(bytes('Fin de connexion !', 'UTF-8'))
+        print('Connexion interrompue.')
+        connexion.close()
+        break
+
+    mySocket.close()
+    del mySocket
